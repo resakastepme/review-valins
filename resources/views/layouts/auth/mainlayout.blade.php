6ee79c16721904858d973f916c4160a64aec2ffb
resakastepme <resa.komara.akbari@gmail.com>
2024-03-03 16:54:16 +0700

    ADDING GOOGLE CONSOLE META

--- resources/views/layouts/auth/mainlayout.blade.php
+++ resources/views/layouts/auth/mainlayout.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <meta name="google-site-verification" content="0iPe0prctv_NK0MZ7eO3wazEXkB5ABvQzMmsN7Y_v4Q" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
